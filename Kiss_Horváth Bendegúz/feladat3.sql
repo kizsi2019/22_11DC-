9641399b8fed6adf8aa47e11b88f1ce0ebc972ec
Bendegúz Kiss-Horváth <bendeguz.kiss-horvath@katonaj-mkszig.hu>
2022-09-14 14:15:28 +0200

    feladat

--- Kiss_Horváth Bendegúz/feladat3.sql
+++ Kiss_Horváth Bendegúz/feladat3.sql
@@ -7,3 +7,6 @@ WHERE Country = "USA" OR Country = "Boston" OR Country = "New Orleans"
 
 SELECT ProductName, Price FROM Products
 ORDER BY Price;
+
+SELECT CategoryName, Description FROM Categories
+WHERE CategoryName = "Produce"
