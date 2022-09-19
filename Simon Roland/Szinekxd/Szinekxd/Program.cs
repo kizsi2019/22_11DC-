3d02e571fb39a6edecde2e43d959618e53b2ef25
Simon Roland <roland.simon@katonaj-mkszig.hu>
2022-09-19 12:26:55 +0200

    orai

---
+++ Simon Roland/Szinekxd/Szinekxd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szinekxd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Ez a szöveg kék ");
+            Console.ForegroundColor = ConsoleColor.Green; 
+            Console.WriteLine("Ez zöld színű szöveg");
+            Console.ForegroundColor = ConsoleColor.Green; 
+            Console.BackgroundColor = ConsoleColor.DarkBlue; 
+            Console.WriteLine("Színes"); 
+            Console.ResetColor(); 
+            Console.WriteLine("Alapértelmezett");
+            Console.BackgroundColor = ConsoleColor.DarkBlue; 
+            Console.Clear();
+            Console.ReadKey();
+        }
+    }
+}
