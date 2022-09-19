88039bafb14604836c3b7abd6a041dd715b1749a
RozsahegyiAkos <akos.rozsahegyi@katonaj-mkszig.hu>
2022-09-19 12:34:22 +0200

    math_fuggvenyek

---
+++ Rózsahegyi Ákos/C#/metodusok/metodusok/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodusok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.BackgroundColor = ConsoleColor.Blue;
+            //Console.WriteLine("Ez kék hátterű szöveg");
+            //Console.ReadKey();
+
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //Console.WriteLine("Ez zöld színű szöveg");
+            //Console.ReadKey()
+
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //Console.BackgroundColor = ConsoleColor.DarkBlue;
+            //Console.WriteLine("Színes");
+            //Console.ResetColor();
+            //Console.ReadKey();
+
+            //Console.BackgroundColor = ConsoleColor.DarkBlue;
+            //Console.Clear();
+            //Console.ReadKey();
+
+            //Console.Beep(400, 1000);
+
+            //Console.Title = "ablak";
+            //Console.ReadKey();
+
+            //int m = Console.WindowHeight, sz = Console.WindowWidth;
+            //Console.WriteLine(m + "*" + sz);
+            //Console.ReadKey();
+
+            //Console.SetWindowSize(1, 1);
+            //Console.ReadKey();
+
+            //int m = Console.WindowHeight;
+            //int sz = Console.WindowWidth;
+            //Console.SetCursorPosition(sz / 2, m / 2);
+            //Console.ReadKey();
+
+
+            //A Math osztály függvényei
+
+            //double x = -20.5;
+            //x = Math.Abs(x); //Megoldás: 20.5
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //double x = 12.6;
+            //x = Math.Ceiling(x); //Megoldás: 13
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //double x = 15;
+            //x = Math.Cos(x); //Megoldás: -0,7596
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //double x = 15.3;
+            //x = Math.Floor(x); //Megoldás: 15
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //double x = 1000;
+            //x = Math.Log(x); //Megoldás: 6,9
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //double x = 1000;
+            //x = Math.Log10(x); //Megoldás: 3
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //byte sz0 = 10, sz1 = 15;
+            //byte max = Math.Max(sz0, sz1); //Megoldás: 15
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //byte sz0 = 10, sz1 = 15;
+            //byte min = Math.Min(sz0, sz1); //Megoldás: 10
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //double alap = 3, kitevo = 4;
+            //double pow = Math.Pow(alap, kitevo); //Megoldás: 81
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //double x = 7.8;
+            //double ker = Math.Round(x); //Megoldás: 8
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //double x = 120;
+            //x = Math.Sin(x); //Megoldás: 0,58
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //double x = 25;
+            //x = Math.Sqrt(x); //Megoldás: 5
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+
+            //double x = 160;
+            //x = Math.Tan(x); //Megoldás: -0,22
+            //Console.WriteLine(x);
+            //Console.ReadKey();
+        }
+    }
+}
