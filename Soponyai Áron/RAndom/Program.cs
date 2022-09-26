93bfcfe5bab54333a687e63564ec0b4e0e753c44
SoponyaiAron <78421666+SoponyaiAron@users.noreply.github.com>
2022-09-26 12:38:00 +0200

    asdasd
    asdad

---
+++ Soponyai Áron/RAndom/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAndom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            for (int i = 1; i<=5; i++)
+            {
+                double dobott = r.Next(1,91);
+                Console.WriteLine("A dobott szám: " + dobott);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
