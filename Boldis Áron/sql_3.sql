50f9366cb96e7b1f521c2bbbb589cd08cd4f0aac
boldisaron <aron.boldis@katonaj-mkszig.hu>
2022-09-28 14:12:44 +0200

    orai

--- Boldis Áron/sql_3.sql
+++ Boldis Áron/sql_3.sql
@@ -3,4 +3,4 @@ SELECT COUNT(employeeId) FROM employee WHERE city = "Seattle";
 b:
 SELECT COUNT(title) FROM employee WHERE title = "Sales Representative";
 c
-SELECT * FROM employee WHERE lastname LIKE "m%"
+SELECT * FROM employee WHERE firstname LIKE "m%"
