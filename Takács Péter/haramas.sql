d567c43689cee72b2c06157f3eae3e10e39f9a3e
Takacs Peter <peter.takacs@katonaj-mkszig.hu>
2022-09-28 14:17:28 +0200

    harmas

---
+++ Takács Péter/haramas.sql
@@ -0,0 +1,9 @@
+a
+SELECT COUNT(city) FROM employee WHERE city = "Seattle";
+b
+SELECT * FROM employee WHERE title = "Sales Representative";
+c
+SELECT COUNT(firstname) FROM employee WHERE firstname like "M%";
+
+
+ 
