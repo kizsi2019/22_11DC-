761534dbbaccfca26adbd2820c4c49059ca5ebc4
boldisaron <aron.boldis@katonaj-mkszig.hu>
2022-09-28 14:06:29 +0200

    orai

---
+++ Boldis Áron/sql_3.sql
@@ -0,0 +1,6 @@
+3. Feladat
+SELECT COUNT(employeeId) FROM employee WHERE city = "Seattle";
+b:
+SELECT COUNT(title) FROM employee WHERE title = "Sales Representative";
+c
+SELECT * FROM employee WHERE lastname LIKE "m%"
