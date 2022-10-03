680c41c928c8c848b7a41ce3d0cd996d0bbb687e
boldisaron <aron.boldis@katonaj-mkszig.hu>
2022-10-03 11:20:48 +0200

    orai

---
+++ Boldis Áron/matrix/matrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[,]
+            {
+                {421, 23, 42, 1},
+                {45, 674, 342, 52},
+                {56, 12, 343, 54 }
+            };
+            Console.WriteLine("Magassága: " + matrix.GetLength(0));
+            Console.WriteLine("Szélessége: " + matrix.GetLength(1));
+            Console.ReadKey();
+
+        }
+    }
+}
