5f1b355ceeae5e8cc695a3bea3629f8c21eac68f
Szabo Levente Tamas <leventetamas.szabo@katonaj-mkszig.hu>
2022-10-03 12:00:50 +0200

    Mátrix2

---
+++ Szabó Levente/Eső/mátrix 2/mátrix 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mátrix_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[3,4];
+            Random r = new Random();
+            for (int i = 1; i < matrix.GetLength(1); i++)
+            {
+                for( int j = 1; j < matrix.GetLength(0); j++)
+                {
+                    matrix[i, j] = r.Next(1, 100);
+                }
+            }
+            for (int i = 0; i <= matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j <= matrix.GetLength(0); j++)
+                {
+                    Console.WriteLine(matrix[i, j] + " ");
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
