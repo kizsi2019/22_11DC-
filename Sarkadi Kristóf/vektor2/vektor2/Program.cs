472e3ed7f931f59450daafcc49738f009886d7f0
kristofstefan.sarkadi@katonaj-mkszig.hu <kristofstefan.sarkadi@katonaj-mkszig.hu>
2022-10-13 14:27:59 +0200

    faladat

---
+++ Sarkadi Kristóf/vektor2/vektor2/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vektor2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+           
+            int[] bevetel = new int[365];
+            int[] kiadas = new int[365];
+            int[] haszon = new int[365];
+            int legjobb = 0;
+
+            for (int i = 1; i < bevetel.Length; i++)
+            {
+             bevetel[i] = r.Next(4000, 12000);
+             kiadas[i] = r.Next(2000, 4000);
+             haszon[i] = bevetel[i] - kiadas[i];
+            if (haszon[i] > legjobb)
+                {
+                    legjobb = haszon[i];
+                }
+
+            }
+            Console.WriteLine("A legjóbb napi bevétel: " + legjobb);
+            Console.ReadKey();
+        }
+    }
+}
