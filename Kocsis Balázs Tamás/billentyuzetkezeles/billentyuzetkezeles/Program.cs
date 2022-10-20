fb03b3fa47302fd6ec18007c5cf61ec25283251c
Kocsis Balázs <kocsis.balazs0506@gmail.com>
2022-10-20 11:02:01 +0200

    1020

---
+++ Kocsis Balázs Tamás/billentyuzetkezeles/billentyuzetkezeles/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace billentyuzetkezeles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ConsoleKey ck = Console.ReadKey().Key;
+            //switch (ck)
+            //{
+            //case ConsoleKey.UpArrow:
+            //Console.WriteLine("A felfele nyilat nyomtad meg!"); break;
+            //case ConsoleKey.DownArrow:
+            //Console.WriteLine("A lefele nyilat nyomtad meg!"); break;
+            //}
+            ConsoleKeyInfo cki;
+            string jelszo = "";
+            do {
+                cki = Console.ReadKey(true);
+                if (cki.Key != ConsoleKey.Enter)
+                { Console.Write("x");
+                    jelszo += cki.KeyChar;
+                } 
+            } 
+            while (cki.Key != ConsoleKey.Enter);
+            if (jelszo == "1234") Console.WriteLine("\nJó jelszó");
+            else 
+                Console.WriteLine("\nHelytelen jelszó");
+
+            Console.ReadKey();
+        }
+    }
+}
