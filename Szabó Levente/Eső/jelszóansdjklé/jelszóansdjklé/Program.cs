58928ded51db4357b7425edf7fce31f96499abb4
Szabo Levente Tamas <leventetamas.szabo@katonaj-mkszig.hu>
2022-10-20 13:48:23 +0200

    m

---
+++ Szabó Levente/Eső/jelszóansdjklé/jelszóansdjklé/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jelszóansdjklé
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+            string jelszo = "";
+            do
+            {
+                cki = Console.ReadKey(true);
+                if (cki.Key != ConsoleKey.Enter)
+                {
+                    Console.Write("x");
+                    jelszo += cki.KeyChar;
+                }
+            } while (cki.Key != ConsoleKey.Enter);
+            if (jelszo == "SajtosSajt") Console.WriteLine("\nJó jelszó");
+            else Console.WriteLine("\nHelytelen jelszó");
+            Console.ReadKey();
+        }
+    }
+}
