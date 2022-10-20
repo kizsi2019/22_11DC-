f238d40bcbbe344272516d8d36da4108002e2440
Bendegúz Kiss-Horváth <bendeguz.kiss-horvath@katonaj-mkszig.hu>
2022-10-20 12:39:38 +0200

    2022,10,20

---
+++ Kiss_Horváth Bendegúz/x_rajzolo/x_rajzolo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x_rajzolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg egy páros számot!");
+            int szam = byte.Parse(Console.ReadLine());
+            int szam2 = szam/2; 
+
+            for (int i = 1; i <= szam2; i++)
+            for (int g = szam2; g <= szam2; g--)
+                {
+
+                
+                int j = i;
+                while (j <= szam2)
+                {
+                    Console.Write("x");
+                    j++;
+                }
+
+                    Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
