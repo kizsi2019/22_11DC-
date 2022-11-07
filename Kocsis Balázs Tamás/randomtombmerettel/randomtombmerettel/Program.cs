dbedeb664f8a6d500bed3fa080ca59dd69c41f96
Kocsis Balázs <kocsis.balazs0506@gmail.com>
2022-11-07 14:25:03 +0100

    1107ora

---
+++ Kocsis Balázs Tamás/randomtombmerettel/randomtombmerettel/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomtombmerettel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int item in RandT(10, 20, 30))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+            }
+            static int[] RandT(int Tmeret, int mettol, int meddig)
+            {
+                Random r = new Random();
+                int[] visszaT = new int[Tmeret];
+                for (int i = 0; i < visszaT.Length; i++)
+                {
+                    visszaT[i] = r.Next(mettol, meddig);
+                }
+                return visszaT;
+            }
+        }
+    }
+
+
+
+
+
+
+
