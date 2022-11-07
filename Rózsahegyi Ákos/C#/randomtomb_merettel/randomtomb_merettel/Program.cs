7ee7c48f5cbc5b65cc83a10c6c22a7b3327625d7
RozsahegyiAkos <akos.rozsahegyi@katonaj-mkszig.hu>
2022-11-07 14:26:48 +0100

    Órai munka (November 7)

---
+++ Rózsahegyi Ákos/C#/randomtomb_merettel/randomtomb_merettel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomtomb_merettel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int item in randt(10, 20, 30))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+
+        static int[] randt(int tmeret, int mettol, int meddig)
+        {
+            Random r = new Random();
+            int[] visszat = new int[tmeret];
+            for (int i = 0; i < visszat.Length; i++)
+            {
+                visszat[i] = r.Next(mettol, meddig);
+            }
+            return visszat;
+        }
+    }
+}
