e4b25cd1a90cf09bc638df56ad2720c36cf7a231
Balint420 <77265532+Balint420@users.noreply.github.com>
2022-11-07 14:25:13 +0100

    Add files via upload

---
+++ Hajdú Bálint/lesson-10/css/Eljarasok/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eljarasok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kiír();
+            int i = Osszeg();
+            Console .WriteLine(i);
+            Console.ReadLine();
+        }
+        static void kiír()
+        {
+            Console.WriteLine("Ez kiír valamit a képernyőre");
+            Console.ReadKey();
+        }
+        static int Osszeg()
+        {
+            return 5 + 7;
+        }
+
+    }
+}
