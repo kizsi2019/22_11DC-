79bc3b6b22b851b2d61e7510fd2f99ba62c95f08
Takacs Peter <peter.takacs@katonaj-mkszig.hu>
2022-11-10 14:24:40 +0100

    asd

---
+++ Takács Péter/C# gyak/rekurzio_faktoriális/rekurzio_faktoriális/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rekurzio_faktoriális
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("6 faktoriális: ");
+            Console.WriteLine(Faktoriális(6));
+            Console.ReadKey();
+        }
+        static ulong Faktoriális(ulong n)
+        {
+            if (n <= 1) return n;
+            return n * Faktoriális(n - 1);
+        }
+    }
+}
