71b62af85ad78dbb1aeb3316c1969a38c02f00c1
RozsahegyiAkos <akos.rozsahegyi@katonaj-mkszig.hu>
2022-11-10 11:29:23 +0100

    Órai munka (November 10)

---
+++ Rózsahegyi Ákos/C#/rekurzio_nemes/rekurzio_nemes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rekurzio_nemes
+{
+    internal class Program
+    {
+        static int variacok_szama = 0;
+        static void Main(string[] args)
+        {
+            int emeletekszama = int.Parse(Console.ReadLine());
+            emeletszine("F", 1, ref emeletekszama);
+            emeletszine("P", 1, ref emeletekszama);
+            emeletszine("Z", 1, ref emeletekszama);
+            Console.WriteLine(variacok_szama);
+            Console.ReadKey();
+        }
+
+        static void emeletszine(string s, int emeletszam, ref int emeletszam_meddig)
+        {
+            if (emeletszam < emeletszam_meddig)
+            {
+                switch (s)
+                {
+                    case "P":
+                        emeletszine("F", emeletszam + 1, ref emeletszam_meddig);
+                        break;
+                    case "F":
+                        emeletszine("F", emeletszam + 1, ref emeletszam_meddig);
+                        emeletszine("P", emeletszam + 1, ref emeletszam_meddig);
+                        emeletszine("Z", emeletszam + 1, ref emeletszam_meddig);
+                        break;
+                    case "Z":
+                        emeletszine("F", emeletszam + 1, ref emeletszam_meddig);
+                        emeletszine("P", emeletszam + 1, ref emeletszam_meddig);
+                        break;
+                }
+            }
+            else variacok_szama++;
+        }
+    }
+}
