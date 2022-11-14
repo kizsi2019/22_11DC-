90cb3db658788f8c09894c756e989d215554e2ee
Sarkadi Kristof <kristofstefan.sarkadi@katonaj-mkszig.hu>
2022-11-14 14:29:21 +0100

    hibakezeles_2022.11.14

---
+++ Sarkadi Kristóf/ref_par_ata_2022.10.27/ref_par_ata_2022.10.27/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ref_par_ata_2022._10._27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool paros;
+            Paros(11, out paros);
+            Console.WriteLine(paros);
+            Console.ReadKey();
+        }
+        static void Paros(int a, out bool log)
+        {
+            if (a % 2 == 0)
+                log = true;
+            else
+                log = false;
+        }
+    }
+}
