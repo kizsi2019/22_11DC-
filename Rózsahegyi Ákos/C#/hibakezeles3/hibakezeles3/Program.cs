88ebdf66652384b7b0005710b7607da106700ee8
RozsahegyiAkos <akos.rozsahegyi@katonaj-mkszig.hu>
2022-11-14 14:26:47 +0100

    Órai munka (November 14)

---
+++ Rózsahegyi Ákos/C#/hibakezeles3/hibakezeles3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hibakezeles3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 0;
+                Console.WriteLine(a / b);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Nullával osztott!");
+            }
+            finally
+            {
+                Console.WriteLine("Vége");
+            }
+            Console.ReadKey();
+        }
+    }
+}
