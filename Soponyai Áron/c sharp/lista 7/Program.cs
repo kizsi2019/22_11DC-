220d84b6c6130d01a025302178e67fcfb9162660
SoponyaiAron <aron.soponyai@katonaj-mkszig.hu>
2022-11-17 13:58:57 +0100

    asd

---
+++ Soponyai Áron/c sharp/lista 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tömb = new int[] { 20, 30, 12 };
+            List<int> lista = new List<int>();
+            lista.AddRange(tömb);
+               for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
