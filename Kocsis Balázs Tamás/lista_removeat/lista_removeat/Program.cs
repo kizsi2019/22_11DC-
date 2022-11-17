baa8469153aea1565ddf8220682216449fdddc92
Kocsis Balázs <kocsis.balazs0506@gmail.com>
2022-11-17 11:25:03 +0100

    1117

---
+++ Kocsis Balázs Tamás/lista_removeat/lista_removeat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_removeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>() { 10, 23, 4 };
+            lista.RemoveAt(2);
+            foreach (var item in lista)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
