a9a7d4cae7d38f177643e06a89ff9d125928c243
Szabo Levente Tamas <leventetamas.szabo@katonaj-mkszig.hu>
2022-11-17 13:48:14 +0100

    dolgozat

---
+++ Szabó Levente/Eső/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>(8);
+            lista.Add(12);
+            lista.Add(43);
+            lista.Add(2);
+            Console.WriteLine("Kapacítása: " + lista.Capacity);
+            Console.WriteLine("Mérete: " + lista.Count);
+        }
+    }
+}
