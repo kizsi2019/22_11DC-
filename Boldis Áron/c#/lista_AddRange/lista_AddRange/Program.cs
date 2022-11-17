8a91bbe04b704cfce7e8f9765807353ce9c3f6c0
boldisaron <aron.boldis@katonaj-mkszig.hu>
2022-11-17 11:24:22 +0100

    -

---
+++ Boldis Áron/c#/lista_AddRange/lista_AddRange/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_AddRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tömb = new int[] { 20, 30, 12 };
+            List<int> lista = new List<int>();
+            lista.AddRange(tömb);
+            foreach (int i in lista)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
