2b1bda4ffbb919768c55c249d833913d7f1f1d4e
Sarkadi Kristof <kristofstefan.sarkadi@katonaj-mkszig.hu>
2022-11-17 11:24:50 +0100

    list_feladatok__2022.XI.17.

---
+++ Sarkadi Kristóf/list2_2022.XI.17/list2_2022.XI.17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list2_2022.XI._17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>(8);
+            lista.Add(12);
+            lista.Add(43);
+            lista.Add(2);
+            Console.WriteLine("Kapacitás: " + lista.Capacity);
+            Console.WriteLine("Mérete: " + lista.Count);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sarkadi Kristóf/list_contain__2022.XI.17/list_contain__2022.XI.17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list_contain__2022.XI._17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>() { "banán", "alma", "körte", "narancs", "kiwi" };
+            lista.Sort();
+            if (lista.Contains("Körte")) Console.WriteLine("Van benne körte");
+            Console.ReadKey();
+        }
+    }
+}
