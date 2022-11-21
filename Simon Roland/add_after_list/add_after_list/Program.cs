27b859e9cbeb4d04a9b0a3ae93fdce4a4d7260b2
Simon Roland <roland.simon@katonaj-mkszig.hu>
2022-11-21 14:29:11 +0100

    orai

---
+++ Simon Roland/add_after_list/add_after_list/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add_after_list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> láncoltlista = new LinkedList<string>();
+            láncoltlista.AddLast("vár");
+            láncoltlista.AddLast("kávé");
+            láncoltlista.AddFirst("autó");
+            LinkedListNode<string> csomópont = láncoltlista.First;
+            láncoltlista.AddAfter(csomópont, "2.hely");
+            Console.WriteLine(csomópont.Value);
+            foreach (string i in láncoltlista)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Simon Roland/linked_remove/linked_remove/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linked_remove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> láncoltlista = new LinkedList<string>();
+            láncoltlista.AddLast("vár");
+            láncoltlista.AddLast("kávé");
+            láncoltlista.AddFirst("autó");
+            láncoltlista.RemoveFirst();
+            foreach (string i in láncoltlista)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
