d0ea831fd16634ae48e9139ad3e6cc54aba740c7
Sarkadi Kristof <kristofstefan.sarkadi@katonaj-mkszig.hu>
2022-11-21 14:31:25 +0100

    lsita__2022.XI.21

---
+++ Sarkadi Kristóf/chain_list__2022.XI.21/chain_list__2022.XI.21/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace chain_list__2022.XI._21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> láncoltlista = new LinkedList<string>();
+            láncoltlista.AddLast("vár");
+            láncoltlista.AddLast("kávé");
+            láncoltlista.AddFirst("autó");
+            foreach (string item in láncoltlista)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
