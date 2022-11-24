d1932bb211d5e9709a1ae4036633d47b15a0dccd
Szabo Levente Tamas <leventetamas.szabo@katonaj-mkszig.hu>
2022-11-24 11:25:01 +0100

    Eső20

---
+++ Szabó Levente/Eső/sorlista/sorlista/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorlista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList sorlista = new ArrayList();
+            sorlista.Add("string"); sorlista.Add(7); sorlista.Add('c');
+            foreach (var item in sorlista)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
