698805eec2ef5e24503be0f37280a2dd35df7ca3
Kocsis Balázs <kocsis.balazs0506@gmail.com>
2022-11-24 11:25:05 +0100

    1124

---
+++ Kocsis Balázs Tamás/verem/verem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            verem.Push(1);
+            verem.Push(2);
+            verem.Push(3);
+            verem.Push(4);
+            foreach (int item in verem)
+            {
+                Console.WriteLine(item);
+            }
+            while (verem.Count != 0)
+            {
+                Console.WriteLine(verem.Pop());
+            }
+            Console.ReadKey();
+        }
+    }
+}
