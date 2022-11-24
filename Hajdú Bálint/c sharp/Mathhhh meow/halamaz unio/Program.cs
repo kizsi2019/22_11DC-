e53f6bfa5eadd284646f6383f21ddbe285de27c0
Balint420 <77265532+Balint420@users.noreply.github.com>
2022-11-24 11:26:53 +0100

    Add files via upload

---
+++ Hajdú Bálint/c sharp/Mathhhh meow/halamaz unio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace halamaz_unio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int>unio=new HashSet<int>(){10,32,4,8};
+            HashSet<int> halmaz1 = new HashSet<int>() { 10, 32, 4, 8 };
+            unio.UnionWith(halmaz1);
+            {
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
