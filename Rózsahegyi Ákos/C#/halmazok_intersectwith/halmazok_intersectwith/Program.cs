e969375d6140fc831a2338c5c763d96fbe42b376
RozsahegyiAkos <akos.rozsahegyi@katonaj-mkszig.hu>
2022-11-24 11:24:08 +0100

    Órai munka (November 24)

---
+++ Rózsahegyi Ákos/C#/halmazok_intersectwith/halmazok_intersectwith/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace halmazok_intersectwith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> alaphalmaz = new HashSet<int>() { 10, 32, 4, 8 };
+            HashSet<int> halmaz1 = new HashSet<int>() { 20, 32, 12, 4 };
+            alaphalmaz.IntersectWith(halmaz1);
+
+            foreach (var item in alaphalmaz)
+            {
+                Console.WriteLine("{0}, ", item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
