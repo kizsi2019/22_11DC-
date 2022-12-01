5dca56188b0556e4cbcea76d8dfcaeac9b0208f8
Kocsis Balázs <kocsis.balazs0506@gmail.com>
2022-12-01 11:28:24 +0100

    1201

---
+++ Kocsis Balázs Tamás/megszamlalaspelda/megszamlalaspelda/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace megszamlalaspelda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randT = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < randT.Length; i++)
+            {
+                randT[i] = r.Next(0, 21);
+            }
+            int db = 0;
+            for (int i = 0; i < randT.Length; i++)
+            {
+                Console.WriteLine(randT[i]);
+                if (randT[i] % 2 == 0) db++;
+            }
+            Console.WriteLine("a generalt tombben {0}db paros szam van", db);
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Kocsis Balázs Tamás/osszegzesproba/osszegzesproba/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osszegzesproba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Lista = new List<int>();
+            StreamReader sr = new StreamReader("díszek.txt");
+            while (!sr.EndOfStream)
+            {
+                Lista.Add(int.Parse(sr.ReadLine()));
+            }
+            sr.Close();
+            int összes = 0;
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                összes+= Lista[i];
+            }
+            Console.WriteLine("Ennyi karácsonyi díszt készítettek: " + összes);
+            Console.ReadKey();
+        }
+    }
+}
