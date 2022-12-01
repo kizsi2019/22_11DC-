44bfcc008788c8722e6684f659a196a3d2b9fa66
Balinzz <balint.pach04@gmail.com>
2022-12-01 14:11:10 +0100

    orai

---
+++ Pach Bálint/eldontes_pelda/eldontes_pelda/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eldontes_pelda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randT = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < randT.Length; i++)
+            {
+                randT[i] = r.Next(0, 21);
+            }
+            int j = 0;
+            while (j < randT.Length && randT[j] >= 15)
+            {
+                j++;
+            }
+            for (int i = 0; i < randT.Length; i++)
+            {
+                Console.WriteLine(randT[i]);
+            }
+            if (j < randT.Length)
+                Console.WriteLine("Van 15-nél nagyobb szám");
+            else Console.WriteLine("Nincs 15-nél nagyobb szám");
+            Console.ReadKey(); 
+        }
+    }
+}
