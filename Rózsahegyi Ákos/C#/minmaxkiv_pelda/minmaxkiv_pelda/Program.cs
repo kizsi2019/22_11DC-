a8531c27576226ad0289dcb3fa4bc9025654efbf
RozsahegyiAkos <akos.rozsahegyi@katonaj-mkszig.hu>
2022-12-01 11:30:46 +0100

    Órai munka (December 1)

---
+++ Rózsahegyi Ákos/C#/minmaxkiv_pelda/minmaxkiv_pelda/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minmaxkiv_pelda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\RozsahegyiAkos\Desktop\GITHUB\22_11DC-\Rózsahegyi Ákos\C#\minmaxkiv_pelda\minmaxkiv_pelda\bin\Debug\július.txt");
+            List<double> hőmérséklet = new List<double>();
+            while (!sr.EndOfStream)
+            {
+                hőmérséklet.Add(double.Parse(sr.ReadLine()));
+            }
+            //Minimum kiválasztás
+            int legalacsonyabb_i = 0;
+            for (int i = 0; i < hőmérséklet.Count; i++)
+            {
+                if (hőmérséklet[i] < hőmérséklet[legalacsonyabb_i]) legalacsonyabb_i = i;
+            }
+            Console.WriteLine("A legalacsonyabb hőmérséklet {0}.-án {1} fok volt!", legalacsonyabb_i + 1,
+            hőmérséklet[legalacsonyabb_i]);
+
+        }
+    }
+}
