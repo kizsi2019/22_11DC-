5d86d6e6ba985d93c203ab925f33b5ce9e63aa94
Simon Roland <roland.simon@katonaj-mkszig.hu>
2022-12-08 11:19:17 +0100

    orai

---
+++ Simon Roland/buborekrendezes/buborekrendezes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buborekrendezes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Tomb = new int[] { 32, 12, 10, 9, 45, 90, 13, 8, 1, 42 };
+            for(int i = Tomb.Length -1; i > 1; i--)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if (Tomb[j] < Tomb[j + 1])
+                    {
+                        int csere = Tomb[j];
+                        Tomb[j] = Tomb[j + 1];
+                        Tomb[j + 1] = csere;
+                    }
+                }
+            }
+            for (int i = 0; i < Tomb.Length; i++)
+            {
+                Console.WriteLine(Tomb[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
