994acd36ffceed99edb66ae2b49dfd8ec2493ac9
RozsahegyiAkos <akos.rozsahegyi@katonaj-mkszig.hu>
2022-12-08 11:06:08 +0100

    Órai munka (December 8)

---
+++ Rózsahegyi Ákos/C#/szetvalogatas_pelda/szetvalogatas_pelda/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szetvalogatas_pelda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\RozsahegyiAkos\Desktop\GITHUB\22_11DC-\Rózsahegyi Ákos\C#\szetvalogatas_pelda\szetvalogatas_pelda\bin\Debug\szamok.txt");
+            int[] szamokt = new int[100];
+            int j = 0;
+            while (!sr.EndOfStream)
+            {
+                szamokt[j] = int.Parse(sr.ReadLine());
+            }
+
+            int[] negyzetszamokt = new int[100];
+            int[] oszthato3szamokt = new int[100];
+            int negyzet_i = 0, osz3_i = 0;
+            for (int i = 0; i < szamokt.Length; i++)
+            {
+                if (szamokt[i] > 0 && Math.Sqrt(szamokt[i]) % 1 == 0)
+                {
+                    negyzetszamokt[negyzet_i] = szamokt[i];
+                    negyzet_i++;
+                }
+                if (szamokt[i] % 3 == 0)
+                {
+                    oszthato3szamokt[osz3_i] = szamokt[i];
+                    osz3_i++;
+                }
+            }
+
+            Console.WriteLine("Négyzetszámok: ");
+            for (int i = 0; i < negyzetszamokt.Length; i++)
+            {
+                Console.WriteLine(negyzetszamokt[i]);
+            }
+            Console.WriteLine("Hárommal oszthatók: ");
+            for (int i = 0; i < oszthato3szamokt.Length; i++)
+            {
+                Console.WriteLine(oszthato3szamokt[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rózsahegyi Ákos/C#/szetvhelyben_tetele/szetvhelyben_tetele/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szetvhelyben_tetele
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[] { 32, 12, 10, 9, 45, 90, 13, 8, 1, 42 };
+            int e = 0;
+            int u = tomb.Length - 1;
+            while (e < u)
+            {
+                int csere = 0;
+                if (tomb[e] > 20)
+                    e++;
+                else
+                {
+                    csere = tomb[e];
+                    tomb[e] = tomb[u];
+                    tomb[u] = csere;
+                    u--;
+                }
+            }
+            Console.WriteLine("Tömb:");
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.WriteLine(tomb[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
