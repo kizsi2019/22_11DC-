ec3268b971e695b065c10ad45e8e267669e2334e
Balint420 <77265532+Balint420@users.noreply.github.com>
2022-12-08 11:27:19 +0100

    órai meloo

---
+++ Hajdú Bálint/c sharp/Mathhhh meow/Szétválogatás/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szétválogatás
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Tömb = new int[] { 32, 12, 10, 9, 45, 90, 13, 8, 1, 42 };
+            int[] FTömb1 = new int[Tömb.Length];
+            int[] FTömb2 = new int[Tömb.Length];
+            int i1 = 0, i2 = 0;
+            for (int i = 0; i < Tömb.Length; i++)
+            {
+                if (Tömb[i] >15)
+                {
+                    FTömb1[i1] = Tömb[i];
+                    i1++;
+                }
+                if (Tömb[i] <9)
+                {
+                    FTömb2[i2] = Tömb[i];
+                    i2++;
+                }
+                
+                Console.WriteLine("FTömb1");
+                foreach (var item in FTömb1)
+                {
+                    Console.WriteLine("FTömb2");
+                    foreach (var item in FTömb2) ;
+                }
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
