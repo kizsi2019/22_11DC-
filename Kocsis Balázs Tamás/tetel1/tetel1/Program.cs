8f4a2093045cb800ac76524992ea9db47c1e051c
Kocsis Balázs <kocsis.balazs0506@gmail.com>
2023-01-12 11:30:44 +0100

    0112

---
+++ Kocsis Balázs Tamás/tetel1/tetel1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tetel1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randT = new int[20];
+            Random r = new Random();
+            for(int i = 0; i < randT.Length; i++) 
+            {
+                randT[i] = r.Next(0, 101);
+            }
+            int paros = 0;
+            foreach (int item in randT)
+            {
+                Console.Write(item + ", ");
+            }
+            for (int i = 0; i < randT.Length; i++) 
+            {
+                
+                if(randT[i] % 2 == 0) paros++;
+            }
+            Console.WriteLine("A generált tömbben {0}db páros szám van", paros);
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
---
+++ Kocsis Balázs Tamás/tetel2/tetel2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tetel2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randT = new int[20];
+            Random r = new Random();
+            for (int i = 0; i < randT.Length; i++)
+            {
+                randT[i] = r.Next(0, 101);
+            }
+            int paros = 0;
+            foreach (int item in randT)
+            {
+                Console.Write(item + ", ");
+            }
+            for (int i = 0; i < randT.Length; i++)
+            {
+
+                if (randT[i] < 20) paros++;
+            }
+            Console.WriteLine("A generált tömbben {0}db 20-nél kisebb szám van", paros);
+            Console.ReadKey();
+        }
+    }
+}
