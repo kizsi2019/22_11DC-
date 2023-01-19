b878c0456d66a5ef0c36bb8a66a5143d4488de96
boldisaron <aron.boldis@katonaj-mkszig.hu>
2023-01-19 13:34:36 +0100

    sql

---
+++ Boldis Áron/c#/abszolut_ertek/abszolut_ertek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abszolut_ertek
+{
+    internal class Program
+    {
+        static int AbszolutErtek(int szam)
+        {
+            return szam > 0 ? szam : szam * -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AbszolutErtek(-10));
+            Console.ReadKey();
+        }
+    }
+}
