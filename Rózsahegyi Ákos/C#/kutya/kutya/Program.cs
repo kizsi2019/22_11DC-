49e014d41d80ecd821690fe2bde77116bc0efe9b
RozsahegyiAkos <akos.rozsahegyi@katonaj-mkszig.hu>
2023-01-23 14:28:51 +0100

    Órai munka (Január 23)

---
+++ Rózsahegyi Ákos/C#/kutya/kutya/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kutya
+{
+    class kutya
+        {
+            public string nev;
+            private int ehsegjelzo = 50;
+            public void Etet(int etel)
+            {
+                ehsegjelzo -= etel;
+            }
+        }
+
+        public void jatek()
+        {
+            if (ehsegjelzo <= 80)
+            {
+                ehsegjelzo += 50;
+                Console.WriteLine("Játék...");
+            }
+            else Console.WriteLine("A kutya éhes, nem tudsz játszani vele!");
+        }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            kutya k = new kutya();
+            k.jatek();
+            k.jatek();
+            Console.ReadKey();
+        }
+    }
+}
