0854dc6e38ca7f28e636627f6a44365fcdaaae7a
boldisaron <aron.boldis@katonaj-mkszig.hu>
2023-01-26 12:24:57 +0100

    '-'

---
+++ Boldis Áron/c#/kutya/kutya/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kutya
+{
+    internal class Program
+    {
+        class Kutya
+        {
+            public string Név;
+            private int ÉhségJelző = 50;
+            public void Etet(int étel)
+            {
+                ÉhségJelző -= étel;
+            }
+            public void Játék()
+            {
+                if (ÉhségJelző <= 80)
+                {
+                    ÉhségJelző += 50;
+                    Console.WriteLine("Játék...");
+                }
+                else Console.WriteLine("A kutya éhes, nem tudsz játszani vele!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Kutya k = new Kutya();
+            k.Játék();
+            k.Játék();
+            k.Név = "fasz";
+            Console.ReadKey();
+        }
+    }
+}
