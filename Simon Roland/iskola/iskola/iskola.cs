b17c268f9562e6add0a1856500f57600d2663f20
Simon Roland <roland.simon@katonaj-mkszig.hu>
2023-02-02 11:30:11 +0100

    orai

--- Simon Roland/iskola/iskola/iskola.cs
+++ Simon Roland/iskola/iskola/iskola.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,13 @@ namespace iskola
             Nev = adatok[2];
             
 
+        }
+        public string azonosito()
+        {
+            char evutolso = Ev.ToString()[3];
+            string veznev = Nev.Substring(0, 3);
+            string kernev = Nev.Split(' ')[1].Substring(0, 3);
+            return (evutolso + Osztaly + veznev + kernev).ToLower();
         }
 
     }
