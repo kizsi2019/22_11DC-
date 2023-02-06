3dfe0f7b875b2db66261071d4237fa1af9c700a1
RozsahegyiAkos <akos.rozsahegyi@katonaj-mkszig.hu>
2023-02-06 13:36:48 +0100

    iskola 3

---
+++ Rózsahegyi Ákos/C#/iskola/iskola/JelszoGeneralo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iskola
+{
+    class JelszoGeneralo
+    {
+        private Random Rnd;
+
+        public JelszoGeneralo(Random r)
+        {
+            Rnd = r;
+        }
+
+        public string Jelszo(int jelszoHossz)
+        {
+            string jelszo = "";
+            while (jelszo.Length < jelszoHossz)
+            {
+                char c = (char)Rnd.Next(48, 123);
+                if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z'))
+                {
+                    jelszo += c;
+                }
+            }
+            return jelszo;
+        }
+    }
+}
