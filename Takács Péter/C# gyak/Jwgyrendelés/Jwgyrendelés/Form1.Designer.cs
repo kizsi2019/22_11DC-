6e50ec130295f0a723b46e38ffb89986d6f002e2
Takacs Peter <peter.takacs@katonaj-mkszig.hu>
2023-03-01 08:05:56 +0100

    asd

---
+++ Takács Péter/C# gyak/Jwgyrendelés/Jwgyrendelés/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Jwgyrendelés
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbttávolság = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.tbtFizetendő = new System.Windows.Forms.Label();
+            this.tbFizetendő = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbOsztály = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jegyrendelés";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(13, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Megtett távolság";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tbttávolság
+            // 
+            this.tbttávolság.Location = new System.Drawing.Point(198, 106);
+            this.tbttávolság.Name = "tbttávolság";
+            this.tbttávolság.Size = new System.Drawing.Size(213, 20);
+            this.tbttávolság.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(14, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Osztály";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(15, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Kedvezmény";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(163, 216);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(113, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "nincs kedvezmény";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(163, 254);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(45, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "50%";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(163, 293);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(45, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.Text = "90%";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // tbtFizetendő
+            // 
+            this.tbtFizetendő.AutoSize = true;
+            this.tbtFizetendő.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbtFizetendő.Location = new System.Drawing.Point(16, 325);
+            this.tbtFizetendő.Name = "tbtFizetendő";
+            this.tbtFizetendő.Size = new System.Drawing.Size(94, 24);
+            this.tbtFizetendő.TabIndex = 9;
+            this.tbtFizetendő.Text = "Fizetendő";
+            // 
+            // tbFizetendő
+            // 
+            this.tbFizetendő.Location = new System.Drawing.Point(132, 330);
+            this.tbFizetendő.Name = "tbFizetendő";
+            this.tbFizetendő.Size = new System.Drawing.Size(279, 20);
+            this.tbFizetendő.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(508, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 249);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Számolás";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbOsztály
+            // 
+            this.tbOsztály.FormattingEnabled = true;
+            this.tbOsztály.Items.AddRange(new object[] {
+            "1.osztály",
+            "2.osztály"});
+            this.tbOsztály.Location = new System.Drawing.Point(198, 155);
+            this.tbOsztály.Name = "tbOsztály";
+            this.tbOsztály.Size = new System.Drawing.Size(213, 21);
+            this.tbOsztály.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbOsztály);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbFizetendő);
+            this.Controls.Add(this.tbtFizetendő);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbttávolság);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Jegyrendelés";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbttávolság;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label tbtFizetendő;
+        private System.Windows.Forms.TextBox tbFizetendő;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox tbOsztály;
+    }
+}
+
