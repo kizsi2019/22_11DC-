0523546e7412dc1dc97db3c6cb28046825623fcc
RozsahegyiAkos <akos.rozsahegyi@katonaj-mkszig.hu>
2023-03-13 13:41:05 +0100

    Toto1

---
+++ Rózsahegyi Ákos/C#/Toto/Toto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = File.ReadAllLines(@"C:\Users\RozsahegyiAkos\Desktop\GITHUB\22_11DC-\Rózsahegyi Ákos\C#\Toto\Toto\bin\Debug\toto.txt");
+            List<totoData> database = new List<totoData>();
+            for (int i = 1; i < inputs.Length; i++)
+            {
+                totoData temp = new totoData(inputs[i]);
+                database.Add(temp);
+            }
+        }
+    }
+}
---
+++ Rózsahegyi Ákos/C#/Toto/Toto/totoData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toto
+{
+    internal class totoData
+    {
+        public int Ev { get; set; }
+        public int Het { get; set; }
+        public int Fordulo { get; set; }
+        public int T13p1 { get; set; }
+        public int Ny13p1 { get; set; }
+        public string Eredmenyek { get; set; }
+
+        public totoData(string line)
+        {
+            string[] splitted = line.Split(' ');
+            Ev = int.Parse(splitted[0]);
+            Het = int.Parse(splitted[1]);
+            Fordulo = int.Parse(splitted[2]);
+            T13p1 = int.Parse(splitted[3]);
+            Ny13p1 = int.Parse(splitted[4]);
+            Eredmenyek = splitted[5];
+        }
+    }
+}
