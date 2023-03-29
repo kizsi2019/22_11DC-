7defc86b7bf010304b333812123ffa653007e90e
Sarkadi kristof <kristofstefan.sarkadi@katonaj-mkszig.hu>
2023-03-29 09:40:45 +0200

    toto

--- Sarkadi Kristóf/Toto_gui/Toto_gui/MainWindow.xaml.cs
+++ Sarkadi Kristóf/Toto_gui/Toto_gui/MainWindow.xaml.cs
@@ -24,5 +24,53 @@ namespace Toto_gui
         {
             InitializeComponent();
         }
+
+        private void Ellenorzes(object send,TextChangedEventArgs e)
+        {
+            int hossz = txbInput.Text.Length;
+            if (hossz != 14)
+            {
+                cbxHossz.IsChecked = true;
+                cbxHossz.Content = $"Nem megfelelő a karakterek száma ({hossz})";
+            }
+            else
+            {
+                cbxHossz.IsChecked = false;
+                cbxHossz.Content = $"Nem megfelelő a karakterek száma ({hossz})";
+            }
+            List<char> invalids = new List<char>();
+            string text = txbInput.Text;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != '1' && text[i] != '2' && text[i] != 'X' && text[i] != 'x')
+                {
+                    invalids.Add(text[i]);
+                }
+            }
+            if (invalids.Count != 0)
+            {
+                cbxKarakter.IsChecked = true;
+                string output = "Helytelen karakter az eredményben(";
+                for (int i = 0; i < invalids.Count -1; i++)
+                {
+                    output += invalids[i] + ";";
+                }
+                output += invalids[invalids.Count - 1] +")";
+                cbxKarakter.Content = output;
+            }
+            else
+            {
+                cbxKarakter.IsChecked = false;
+                cbxKarakter.Content = "Helytelen karakter az eredményben()";
+            }
+            if (cbxKarakter.IsChecked == true || cbxKarakter.Checked == true)
+            {
+                btnMentes.IsEnabled = false;
+            }
+            else
+            {
+                btnMentes.IsEnabled = true;
+            }
+        }
     }
 }
