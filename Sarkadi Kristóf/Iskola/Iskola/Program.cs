b643da62189029fb4ad39c1b52d7f1dd09a073a4
Sarkadi Kristof Stefan <kristofstefan.sarkadi@katonaj-mkszig.hu>
2023-04-03 13:39:33 +0200

    Iskola

---
+++ Sarkadi Kristóf/Iskola/Iskola/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Iskola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tanulo> tanulok = new List<Tanulo>();
+            StreamReader sr = new StreamReader("nevek.txt");
+            while(!sr.EndOfStream)
+            {
+                tanulok.Add(new Tanulo(sr.ReadLine()));
+            }
+            sr.Close();
+            Console.WriteLine($"3.feladat: Az iskolában {tanulok.Count} tanuló jár");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sarkadi Kristóf/Iskola/Tanulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace if
+{
+	class Tanulo
+	{
+		public int Ev { get; set; }
+		public string Osztaly { get; set; }
+		public string Nev { get; set; }
+	
+		public Tanulo(string sor)
+		{
+			string[] adatok = sor.Split(';');
+			Ev = int.Parse(adatok[0]);
+			Osztaly = adatok[1];
+			Nev = adatok[2];
+		}
+		
+		public int NevHossza
+		{
+			get
+			{
+				return Nev.Replace(" ", "").Length;
+			}
+		}
+	}
+}
