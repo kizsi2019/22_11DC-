ee104c4171f5c4384a7cf774e3099802d7f80061
Simon Roland <roland.simon@katonaj-mkszig.hu>
2023-04-03 13:38:52 +0200

    asd

---
+++ Simon Roland/iskola1/iskola1/Tanulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iskola1
+{
+    internal class Tanulo
+    {
+        public int Ev { get; set; }
+        public string Osztaly { get; set; }
+        public string Nev { get; set; }
+        public Tanulo(sor)
+        {
+            string[] adatok = sor.split(";");
+            Ev = int.Parse(adatok[0]);
+            Osztaly = adatok[1];
+            Nev = adatok[2];
+        }
+    }
+}
