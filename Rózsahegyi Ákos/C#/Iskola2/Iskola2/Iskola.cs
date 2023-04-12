7f467a9c85e3fb43e3402e03d29814569f431c13
Rozsahegyi Akos <akos.rozsahegyi@katonaj-mkszig.hu>
2023-04-12 09:13:44 +0200

    IskolaWIP

---
+++ Rózsahegyi Ákos/C#/Iskola2/Iskola2/Iskola.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iskola2
+{
+    internal class Iskola
+    {
+        public int Ev { get; set; }
+        public string Osztaly { get; set; }
+        public string Nev { get; set; }
+
+        public Iskola(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            Ev = int.Parse(adatok[0]);
+            Osztaly = adatok[1];
+            Nev = adatok[2];
+        }
+
+        public int NevHossza
+        {
+            get
+            {
+                return Nev.Replace(" ", "").Length;
+            }
+        }
+    }
+}
---
+++ Rózsahegyi Ákos/C#/Iskola2/Iskola2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Iskola2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Iskola> tanulok = new List<Iskola>();
+            StreamReader sr = new StreamReader("nevek.txt");
+            while (!sr.EndOfStream)
+            {
+                tanulok.Add(new Iskola(sr.ReadLine()));
+            }
+            sr.Close();
+
+            Console.WriteLine($"3. feladat: Az iskolába {tanulok.Count} tanuló jár");
+        }
+    }
+}
