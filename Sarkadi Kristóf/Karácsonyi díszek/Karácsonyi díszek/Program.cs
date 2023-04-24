ed515f1827c3d835817c6772b7151f080c065521
Sarkadi Kristof <kristofstefan.sarkadi@katonaj-mkszig.hu>
2023-04-24 13:35:38 +0200

    karacsonyi_disz

---
+++ Sarkadi Kristóf/Karácsonyi díszek/Karácsonyi díszek/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Karácsonyi_díszek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<NapiMunka> napiMunkaLista = new List<NapiMunka>();
+            foreach(string sor in File.ReadLines("diszek.txt"))
+            {
+                napiMunkaLista.Add(new NapiMunka(sor));
+            }
+            Console.WriteLine($"4.feladat: Összesen {NapiMunka.KeszultDb} darab dísz készült");
+            bool nemKeszultDisz = false;
+            foreach (NapiMunka disz in napiMunkaLista)
+            {
+                if (disz.HarangKesz == 0 && disz.AngyalkaKesz == 0 && disz.FenyofaKesz == 0)
+                {
+                    nemKeszultDisz = true;
+                    break;
+                }
+            }
+            if (nemKeszultDisz)
+            {
+                Console.WriteLine("5.feladat: Volt olyan nap, amikor egyetlen dísz sem készült");
+            }
+            else
+            {
+                Console.WriteLine("5.feladat: Nem volt olyan nam, amikor egyetlen dísz sem készült" );
+            }
+            Console.ReadKey();
+        }
+    }
+}
