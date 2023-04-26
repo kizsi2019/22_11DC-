06e94bc88c6ff9f2e79a7d2d88990963218c5d48
DubeczDavid <davidraymundo.dubecz@katonaj-mkszig.hu>
2023-04-26 07:00:46 +0200

    potolva

---
+++ Dubecz Dávid/C#/potolaskaracsony/potolaskaracsony/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karacsonyCLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<NapiMunka> napiMunkaLista = new List<NapiMunka>();
+            StreamReader sr = new StreamReader("diszek.txt");
+            while (!sr.EndOfStream)
+            {
+                napiMunkaLista.Add(new NapiMunka(sr.ReadLine()));
+            }
+            Console.WriteLine("4. feladat: Összesen {0} darab dísz készült.", NapiMunka.KeszultDb);
+            
+            bool nemKeszultDisz = false;
+            foreach (NapiMunka disz in napiMunkaLista)
+            {
+                if (disz.HarangKesz == 0 && disz.AngyalkaKesz == 0 && disz.FenyofaKesz == 0)
+                {
+                    nemKeszultDisz = true;
+                    break;
+                }
+            }
+            if (nemKeszultDisz)
+            {
+                Console.WriteLine("5. feladat: Volt olyan nap, amikor egyetlen dísz sem készült.");
+            }
+            else
+            {
+                Console.WriteLine("5. feladat: Nem volt olyan nap, amikor egyetlen dísz sem készült.");
+            }
+            Console.ReadKey();
+            sr.Close();
+        }
+    }
+}
