d67d9e1b6707508e992683b21e7faec3671399ba
Sarkadi Kristof <kristofstefan.sarkadi@katonaj-mkszig.hu>
2023-05-12 10:35:22 +0200

    uzenet_fal

---
+++ Sarkadi Kristóf/webapp/uzenet_fal.py
@@ -0,0 +1,15 @@
+from flask import  Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "Üdv a Sulipy oldalon!"
+
+@app.route('/contact')
+def contact():
+    return f'Jelentkezés a kurzusokra: info@sulipy.hu'
+
+if __name__ == "__main__":
+    app.run(debug=True)
