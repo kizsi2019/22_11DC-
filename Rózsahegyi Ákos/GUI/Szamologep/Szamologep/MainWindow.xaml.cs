0b8fb9adc561f1e578bd515b9f6b26c89f2a01ff
Rozsahegyi Akos <akos.rozsahegyi@katonaj-mkszig.hu>
2023-05-15 13:39:14 +0200

    Számoló 2023-05-15

--- Rózsahegyi Ákos/GUI/Szamologep/Szamologep/MainWindow.xaml.cs
+++ Rózsahegyi Ákos/GUI/Szamologep/Szamologep/MainWindow.xaml.cs
@@ -20,6 +20,11 @@ namespace Szamologep
     /// </summary>
     public partial class MainWindow : Window
     {
+        string first = "";
+        string second = "";
+        char function;
+        double result = 0.0;
+        string userInput = "";
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +33,152 @@ namespace Szamologep
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void BtnPercent_Click(object sender, RoutedEventArgs e)
+        {
+            function = '%';
+        }
+
+        private void Btn0_Click(object sender, RoutedEventArgs e)
+        {
+            TBox.Text += "";
+            userInput += "0";
+            TBox.Text = userInput;
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            TBox.Text += "";
+            userInput += "1";
+            TBox.Text = userInput;
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            TBox.Text += "";
+            userInput += "2";
+            TBox.Text = userInput;
+        }
+
+        private void Btn3_Click(object sender, RoutedEventArgs e)
+        {
+            TBox.Text += "";
+            userInput += "3";
+            TBox.Text = userInput;
+        }
+
+        private void Btn4_Click(object sender, RoutedEventArgs e)
+        {
+            TBox.Text += "";
+            userInput += "4";
+            TBox.Text = userInput;
+        }
+
+        private void Btn5_Click(object sender, RoutedEventArgs e)
+        {
+            TBox.Text += "";
+            userInput += "5";
+            TBox.Text = userInput;
+        }
+
+        private void Btn6_Click(object sender, RoutedEventArgs e)
+        {
+            TBox.Text += "";
+            userInput += "6";
+            TBox.Text = userInput;
+        }
+
+        private void Btn7_Click(object sender, RoutedEventArgs e)
+        {
+            TBox.Text += "";
+            userInput += "7";
+            TBox.Text = userInput;
+        }
+
+        private void Btn8_Click(object sender, RoutedEventArgs e)
+        {
+            TBox.Text += "";
+            userInput += "8";
+            TBox.Text = userInput;
+        }
+
+        private void Btn9_Click(object sender, RoutedEventArgs e)
+        {
+            TBox.Text += "";
+            userInput += "9";
+            TBox.Text = userInput;
+        }
+
+        private void BtnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            function = '/';
+            first = userInput;
+            userInput = "";
+        }
+
+        private void BtnX_Click(object sender, RoutedEventArgs e)
+        {
+            function = '*';
+            first = userInput;
+            userInput = "";
+        }
+
+        private void BtnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            function = '+';
+            first = userInput;
+            userInput = "";
+        }
+
+        private void BtnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            function = '-';
+            first = userInput;
+            userInput = "";
+        }
+
+        private void BtnSlash_Click(object sender, RoutedEventArgs e)
+        {
+            TBox.Text += "";
+            userInput += ",";
+            TBox.Text = userInput;
+        }
+
+        private void BtnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            second = userInput;
+            double firstNum, secondNum;
+            firstNum = Convert.ToDouble(first);
+            secondNum = Convert.ToDouble(second);
+
+            if (function == '+')
+            {
+                result = firstNum + secondNum;
+                TBox.Text = result;
+            }
+            else if (function == '-')
+            {
+                result = firstNum - secondNum;
+                TBox.Text = result;
+            }
+            else if (function == '*')
+            {
+                result = firstNum * secondNum;
+                TBox.Text = result;
+            }
+            else if (function == '/')
+            {
+                if (secondNum == 0)
+                {
+                    TBox.Text = "Nullával nem lehet osztani";
+                }
+                else
+                {
+                    result = firstNum / secondNum;
+                    TBox.Text = result;
+                }
+            }
         }
     }
 }
