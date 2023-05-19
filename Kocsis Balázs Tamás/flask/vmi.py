a83893aa5cc78affe6a16ff8648e1d41733a4d8d
Kocsis Balazs Tamas <balazstamas.kocsis@katonaj-mkszig.hu>
2023-05-19 13:30:11 +0200

    EGDWDG

---
+++ Kocsis Balázs Tamás/flask/vmi.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, url_for
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/message_board')
+def message_board():
+    return redirect(url_for('home'))
+
+@app.route('/course/<int:number>')
+def course(number):
+    return f'Ez a {number} kurzus adatlapja.'
+
+@app.route('/contact')
+def contact():
+    return 'Jelentkezés erre a kurzusra: info@sulipy.hu'
+
+@app.route('/time')
+def time():
+    now = datetime.now()
+    current_time = now.strftime('%H:%M')
+    return 'A pontos idő: ' + current_time
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Kocsis Balázs Tamás/szamologep/szamologep/MainWindow.xaml.cs
+++ Kocsis Balázs Tamás/szamologep/szamologep/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,151 @@ namespace szamologep
     /// </summary>
     public partial class MainWindow : Window
     {
+        string first = "";
+        string second = "";
+        char function;
+        double result = 0.0;
+        string userInput = "";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void egybtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void kettobtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void harombtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void szorzasbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void negybtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void otbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void hatbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void minuszbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void hetbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nyolcbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void kilencbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pluszbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nullabtn_Click(object sender, RoutedEventArgs e)
+        {
+            outtb.Text += "";
+            userInput += "7";
+            outtb.Text = userInput;
+        }
+
+        private void vesszobtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void egyenlobtn_Click(object sender, RoutedEventArgs e)
+        {
+            second = userInput;
+            double firstNUM, secondNUM;
+            firstNUM = Convert.ToDouble(first);
+            secondNUM = Convert.ToDouble(second);
+
+            if (function == '+')
+            {
+                result = firstNUM + secondNUM;
+                outtb.Text = result;
+            }
+            else if (function == '-')
+            {
+                result = firstNUM - secondNUM;
+                outtb.Text = result;
+            }
+            else if (function == '*')
+            {
+                result = firstNUM * secondNUM;
+                outtb.Text = result;
+            }
+
+            else if (function == '/')
+            {
+                if (secondNUM == 0)
+                {
+                    outtb.Text = "Nullával nem lehet osztani";
+                }
+                else
+                {
+                    result = firstNUM / secondNUM;
+                    outtb.Text = result;
+                }
+            }
+        }
+
+        private void osztasbtn_Click(object sender, RoutedEventArgs e)
+        {
+            function = '/';
+            first = userInput;
+            userInput = "";
+        }
+
+        private void bsbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clearbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void szazalekbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void outtb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
