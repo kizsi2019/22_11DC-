4fcc7655590a572730f7d632fba1dc9ec56aeab3
Levente20k <113507019+Levente20k@users.noreply.github.com>
2023-05-30 14:27:34 +0200

    Endgame

--- Szabó Levente/minden/py/spotifirys.py
+++ Szabó Levente/minden/py/spotifirys.py
@@ -1,6 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = '0faadf6bd5f2417c9fdba7a5abf0a08d'
-client_secret = 'b1d5c4e7d84d4c5cb1facd681643834c'
+client_id = '6rqhFgbbKwnb9MLmUQDhG6'
+client_secret = '6rqhFgbbKwnb9MLmUQDhG6'
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+
+sp.search()
