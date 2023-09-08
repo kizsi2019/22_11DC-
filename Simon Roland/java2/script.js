c044798902b6a5c7db879d4b4f109e70c514eec1
Simon Roland <roland.simon@katonaj-mkszig.hu>
2023-09-08 09:36:29 +0200

    orai

---
+++ Simon Roland/java2/script.js
@@ -0,0 +1,13 @@
+var keresztnev$Nev = "Pisti";
+var a = 2;
+console.log(keresztnev$Nev);
+var vezeteknev = "Kis";
+console.log(vezeteknev);
+var kor = 32;
+ var nagykoru = false;
+ console.log(nagykoru);
+ var magassag;
+ console.log(magassag);
+ console.log(nemLetezo);
+var magassag;
+console.log(magassag)
---
+++ Simon Roland/java3/script.js
@@ -0,0 +1,26 @@
+/*var keresztnev$Nev = "Pisti";
+var a = 2;
+console.log(keresztnev$Nev);
+var vezeteknev = "Kis";
+console.log(vezeteknev);
+var kor = 32;
+ var nagykoru = false;
+ console.log(nagykoru);
+ var magassag;
+ console.log(magassag);
+ console.log(nemLetezo);
+var magassag;
+console.log(magassag)*/
+
+var keressztNev = "Pisti"
+var kor = 32;
+
+console.log(keressztNev + ' ' + kor);
+
+var vezetekNev, felnott;
+vezetekNev = "Kovács";
+
+console.log(vezetekNev + ' felnőtt? ' + felnott);
+
+kor = "harminc"
+
