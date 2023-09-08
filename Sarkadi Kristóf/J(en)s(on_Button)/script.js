53170cd6fcd6e0a9b951f25aba1e2f09a5396940
Sarkadi Kristof Stefan <kristofstefan.sarkadi@katonaj-mkszig.hu>
2023-09-08 09:36:47 +0200

    java_script

---
+++ Sarkadi Kristóf/J(en)s(on_Button)/script.js
@@ -0,0 +1,23 @@
+//Matek operátorok
+var szam = 10;
+var osszeg = szam + 33;
+console.log(osszeg);
+var kulonbseg = 100 - szam;
+console.log(kulonbseg);
+
+console.log(szam * 2);
+console.log(szam / 3);
+
+//Logikai op.
+var szam1 = 100;
+var szam2 = 100;
+var egyenlo = szam1 <= szam2;
+console.log(egyenlo);
+
+//typeof
+console.log(typeof szam1);
+console.log(typeof 'jfksdjfksdk')
+console.log(typeof egyenlo);
+
+var a;
+console.log(typeof a);
