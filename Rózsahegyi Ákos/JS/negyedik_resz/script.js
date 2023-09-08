b7e1c3510b6c35bee0d40ef8931c282f7df3e362
Rozsahegyi Akos <akos.rozsahegyi@katonaj-mkszig.hu>
2023-09-08 09:39:06 +0200

    Javascript 1-5 2023-09-08

---
+++ Rózsahegyi Ákos/JS/negyedik_resz/script.js
@@ -0,0 +1,56 @@
+/*var keresztNev = "Pisti";
+var vezetekNev = "Kis";
+var a = 2;
+console.log(keresztNev);
+console.log(vezetekNev);
+
+var kor = 32;
+console.log(kor);
+
+var nagykoru = false;
+console.log(nagykoru);
+
+var magassag;
+console.log(magassag);
+console.log(nemLetezo);*/
+
+//you stink jk
+
+/*var keresztNev = "Pisti";
+var kor = 32;
+
+console.log(keresztNev + ' ' + kor);
+
+var vezetekNev, felnott;
+vezetekNev = "Kovács";
+//felnott = true;
+
+console.log(vezetekNev + ' felnőtt? ' + felnott);
+
+kor = "harminc";*/
+
+
+//Matematikai operátorok
+var szam = 10;
+var osszeg = szam + 33;
+console.log(osszeg);
+var kulonbseg = 100 - szam;
+console.log(kulonbseg);
+
+console.log(szam * 2);
+console.log(szam / 3);
+
+
+//Logikai operátorok
+var szam1 = 100;
+var szam2 = 100;
+var egyenlo = szam1 <= szam2;
+console.log(egyenlo);
+
+// typeof
+console.log(typeof szam1);
+console.log(typeof "tököm kukim");
+console.log(typeof egyenlo);
+
+var a;
+console.log(typeof a);
---
+++ Rózsahegyi Ákos/JS/otodik_resz/script.js
@@ -0,0 +1,13 @@
+// var, let, const
+
+var nev = "Pistike";
+
+
+function teszt() {
+    var x = 10;
+    //console.log(x);
+}
+
+teszt();
+
+console.log(x);
