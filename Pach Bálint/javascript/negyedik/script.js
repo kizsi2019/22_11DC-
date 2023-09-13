ad5f25228f208fba8afcdcb0c959c482f863a43b
Pach Balint <balint.pach@katonaj-mkszig.hu>
2023-09-13 12:31:11 +0200

    orai

---
+++ Pach Bálint/javascript/negyedik/script.js
@@ -0,0 +1,15 @@
+var szam = 10;
+var osszeg = szam + 33;
+console.log(osszeg);
+var kulonbseg = 100 - szam;
+console.log(kulonbseg);
+
+console.log(szam * 2);
+console(szam / 3);
+
+var szam1 = 100;
+var szam2 = 100;
+var egyenlo = szam1 <= szam2;
+console.log(egyenlo);
+
+console.log(typeof szam1)
