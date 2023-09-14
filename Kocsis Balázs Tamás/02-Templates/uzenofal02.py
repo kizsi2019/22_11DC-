33a60a1646e168d564ec9efdab8dd7046a3fa817
KocsisBalazsTamas <kocsis.balazs0506@gmail.com>
2023-09-14 10:32:38 +0200

    asdax

--- Kocsis Balázs Tamás/02-Templates/uzenofal02.py
+++ Kocsis Balázs Tamás/02-Templates/uzenofal02.py
@@ -3,28 +3,34 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('index.html')
 
+
 @app.route('/message_board')
 def message_board():
     return redirect(url_for('home'))
 
+
 @app.route('/course/<int:number>')
 def course(number):
-    return f'Ez a {number} kurzus adatlapja.'
+    return f'Ez a {number}. kurzus adatlapja.'
+
 
 @app.route('/contact')
 def contact():
-    return 'Jelentkezés erre a kurzusra: info@sulipy.hu'
+    return render_template('contact.html')
+
 
 @app.route('/time')
 def time():
     now = datetime.now()
-    current_time = now.strftime('%H:%M')
-    return 'A pontos idő: ' + current_time
+    current_time = now.strftime('%H:%S')
+    return 'A pontos idő' + current_time
+
 
 if __name__ == '__main__':
     app.run(debug=True)
